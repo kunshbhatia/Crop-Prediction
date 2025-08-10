e4b29530db803c4cb30b76d7b317428a7d651e40
Kunsh Bhatia <kunshcomp@gmail.com>
2025-08-10 11:22:58 +0530

    Update Temp_RH_Rain.py

--- Programs/Temp_RH_Rain.py
+++ Programs/Temp_RH_Rain.py
@@ -10,8 +10,8 @@ def get_weather_info(lat, lon):
 
     try : 
         # Get date range: past 200 days
-        end_date = datetime.now() - timedelta(days=4) # Getting avg of past 1 months 
-        start_date = end_date - timedelta(days= 34)
+        end_date = datetime.now() - timedelta(days=4) # Getting avg of past 6 months 
+        start_date = end_date - timedelta(days= 185)
         start_str = start_date.strftime("%Y%m%d")
         end_str = end_date.strftime("%Y%m%d")
 
