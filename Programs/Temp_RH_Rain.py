8bd4bff77af1a5f63a9bb14a9b3aa3e9c62a5e1f
Kunsh Bhatia <kunshcomp@gmail.com>
2025-08-10 11:41:18 +0530

    Update Temp_RH_Rain.py

--- Programs/Temp_RH_Rain.py
+++ Programs/Temp_RH_Rain.py
@@ -10,14 +10,14 @@ def get_weather_info(lat, lon):
 
     try : 
         # Get date range: past 200 days
-        end_date = datetime.now() - timedelta(days=370) # Getting avg of past 2 months 
-        start_date = end_date - timedelta(days= 732)
-        start_str = start_date.strftime("%Y")
-        end_str = end_date.strftime("%Y")
+        end_date = datetime.now() - timedelta(days=4) # Getting avg of past 1 months 
+        start_date = end_date - timedelta(days= 34)
+        start_str = start_date.strftime("%Y%m%d")
+        end_str = end_date.strftime("%Y%m%d")
 
         # Build API request
         url = (
-            "https://power.larc.nasa.gov/api/temporal/monthly/point"
+            "https://power.larc.nasa.gov/api/temporal/daily/point"
             "?parameters=T2M,RH2M,PRECTOTCORR"
             f"&community=AG&latitude={lat}&longitude={lon}"
             f"&start={start_str}&end={end_str}&format=JSON"
@@ -34,7 +34,7 @@ def get_weather_info(lat, lon):
         return {
             "Average Temperature" : round(sum(Temperature_List)/len(Temperature_List) , 2 ),
             "Average Relative Humidity" : round(sum(RH_list)/len(RH_list),2),
-            "Average Rain" : round((sum(Rain_list)/len(Rain_list))*60, 2) 
+            "Average Rain" : round(((sum(Rain_list)/len(Rain_list))*30), 2) 
         }
     
     except:
