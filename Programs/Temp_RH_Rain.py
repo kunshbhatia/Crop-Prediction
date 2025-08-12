8a6f618848fd4a4ae34c9ab888e676c67a7201c1
Kunsh Bhatia <kunshcomp@gmail.com>
2025-08-12 20:25:22 +0530

    Update Temp_RH_Rain.py

--- Programs/Temp_RH_Rain.py
+++ Programs/Temp_RH_Rain.py
@@ -11,26 +11,33 @@ def get_weather_info(lat, lon):
     try : 
 
         
-        year = 2023
-        
-        url = (
-            "https://power.larc.nasa.gov/api/temporal/daily/point"
-            "?parameters=T2M,RH2M,PRECTOTCORR"
-            f"&community=AG&latitude={lat}&longitude={lon}"
-            f"&start={year}0101&end={year}1231&format=JSON"
-        )
+        year = 2019,2020,2021,2022,2023,2024
+        TEMP = []
+        RH = []
+        RAIN = []
+        for i in year:
+            url = (
+                "https://power.larc.nasa.gov/api/temporal/daily/point"
+                "?parameters=T2M,RH2M,PRECTOTCORR"
+                f"&community=AG&latitude={lat}&longitude={lon}"
+                f"&start={year}0101&end={year}1231&format=JSON"
+            )
+    
+            response = requests.get(url)
+            data = response.json()
+            Temperature_List = list(data['properties']['parameter']['T2M'].values())
+            RH_list = list(data['properties']['parameter']["RH2M"].values())
+            Rain_list = list(data['properties']['parameter']['PRECTOTCORR'].values())
 
-        response = requests.get(url)
-        data = response.json()
-        Temperature_List = list(data['properties']['parameter']['T2M'].values())
-        RH_list = list(data['properties']['parameter']["RH2M"].values())
-        Rain_list = list(data['properties']['parameter']['PRECTOTCORR'].values())
+            TEMP.append(Temperature_List)
+            RH.append(RH_list)
+            RAIN.append(Rain_list)
 
         #Made by Kunsh Bhatia 
         return {
-            "Average Temperature" : round(sum(Temperature_List)/len(Temperature_List) , 2 ),
-            "Average Relative Humidity" : round(sum(RH_list)/len(RH_list),2),
-            "Average Rain" : round((sum(Rain_list)/10), 2) 
+            "Average Temperature" : round(sum(TEMP)/len(TEMP) , 2 ),
+            "Average Relative Humidity" : round(sum(RH)/len(RH),2),
+            "Average Rain" : round((sum(RAIN)/60), 2) 
         }
     
     except:
