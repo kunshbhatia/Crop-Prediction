ec714a224ed432a16cfd8f6cf4d1bd887adb21bc
Kunsh Bhatia <kunshcomp@gmail.com>
2025-08-22 20:30:43 +0530

    Update Temp_RH_Rain.py

--- Programs/Temp_RH_Rain.py
+++ Programs/Temp_RH_Rain.py
@@ -10,9 +10,8 @@ def get_weather_info(lat, lon):
 
     try : 
 
-
-        end_date = datetime.now() - timedelta(days=4) # Getting avg of past 3 months 
-        start_date = end_date - timedelta(days= 97)
+        end_date = datetime.now() - timedelta(days=275) # Getting avg of past 3 months 
+        start_date = datetime.now() - timedelta(days=365)
         start_str = start_date.strftime("%Y%m%d")
         end_str = end_date.strftime("%Y%m%d")
 
